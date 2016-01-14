40ea6c1d7cc9fd5f697345747f26526084cc4ea7
Gregory Casamento <greg.casamento@gmail.com>
2016-01-14 12:59:51 -0500

    Initial Commit

---
+++ xibanalyzer/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  xibanalyzer
+//
+//  Created by Gregory Casamento on 1/14/16.
+//  Copyright © 2016 GNUstep. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
